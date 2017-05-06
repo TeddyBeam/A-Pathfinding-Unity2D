4d8e134b7d4cc467703de126cbb40c104154c5a0
Lý Đông Cảnh <keshiki.ldc@gmail.com>
2017-05-06 13:49:43 +0700

    Add files via upload

---
+++ A-starAlgorithm/IHeapItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PathFinding
+{
+    public interface IHeapItem<T> : IComparable<T>
+    {
+        int HeapIndex
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ A-starAlgorithm/Node.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PathFinding
+{
+    public class Node : IHeapItem<Node>
+    {
+        #region Init
+        public bool walkable;
+        public Vector2 worldPosition;
+        public int gridX;
+        public int gridY;
+
+        public int gCost;
+        public int hCost;
+        public Node parent;
+        private int heapIndex;
+
+        public int HeapIndex
+        {
+            get
+            {
+                return heapIndex;
+            }
+            set
+            {
+                heapIndex = value;
+            }
+        }
+
+        public int fCost
+        {
+            get
+            {
+                return gCost + hCost;
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        public Node(bool _walkable, Vector2 _worldPos, int _gridX, int _gridY)
+        {
+            walkable = _walkable;
+            worldPosition = _worldPos;
+            gridX = _gridX;
+            gridY = _gridY;
+        }
+
+        public int CompareTo(Node nodeToCompare)
+        {
+            int compare = fCost.CompareTo(nodeToCompare.fCost);
+            if (compare == 0)
+            {
+                compare = hCost.CompareTo(nodeToCompare.hCost);
+            }
+            return -compare;
+        }
+        #endregion
+    }
+}
